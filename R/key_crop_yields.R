f4f6f7f2c4429ac1200960f83baa182503af32b8
Helen Evangelina <heva0001@student.monash.edu>
2020-09-16 17:06:17 +1000

    Adding data documentation

---
+++ R/key_crop_yields.R
@@ -0,0 +1,11 @@
+#' Global key crop yields
+#'
+#' This dataset contains crop yields from 249 countries.
+#'
+#'
+#' @param
+#'
+#' @source
+#' This data below comes from Our World in Data and sourced from Tidy Tuesday.
+#'
+"key_crop_yields"
